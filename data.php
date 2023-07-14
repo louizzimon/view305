bf8eecff47677d902721c10257b6dbc042c6ae10
idfabrik <idfabrik@gmail.com>
2023-07-14 12:33:16 +0200

    Update data.php

--- data.php
+++ data.php
@@ -9,16 +9,23 @@ $chap_01["titre"]="L'Atelier / TRANS305 s'installe au coeur du chantier";
 $chap_01["video_intro"]["youtube"]="iwcRbM6FfN8"; // // video youtube - code de la vidéo
 
 $chap_01[vignette][1]["titre"]="Livreur de prédalle";
-$chap_01[vignette][1]["video"]="";
-
-$chap_01[vignette][1]["titre"]="Livreur de prédalle";
 $chap_01[vignette][1]["sous-titre"]="La sécurité, t'as compris ?";
 $chap_01[vignette][1]["slider"]="chap01/img_slider_predalle/";
-
+$chap_01[vignette][1]["couleur"]="";
 
 
 $chap_02["video_intro"]["titre"]="Le montage est terminé. L'atelier accueil habitants, étudiants et artistes";
 $chap_02["video_intro"]["youtube"]="eF5r25aqZTw";
+
+
+$chap_02[vignette][1]["titre"]="Effet cocktail party";
+$chap_02[vignette][1]["sous-titre"]="Résidence et concert d'Antoirer Bellanger";
+$chap_02[vignette][1]["youtube"]="";
+$chap_02[vignette][1]["couleur"]="";
+
+
+
+
 $chap_03["video_intro"]="F2HXAoVd-XA";
 
 
