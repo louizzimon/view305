2e6bb790139b64385d26261e3eaf1ccee4bf5dac
Tawan Arun <idfabrik@gmail.com>
2023-07-14 15:25:55 +0200

    update Data structure + add info to readme

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# view305
+# Views305
+
+Adaptation du web-documentaire Views306 en HTML/PHP, l'ancien site qui avait été développé en Flash.
+
+## Fonctionnement / architecture
+L'ensemble des vidéos est externalisé et se trouve sur YouTube.
+Les images, les typos et autres ressources se trouvent sur le serveur de publication et doivent être accessibles depuis le script PHP.
+Un script PHP sert à générer une page HTML qui contient tous les contenus du projet dans un seul long fichier statique.
+Cette page (index.html) est prévue pour continuer à fonctionner sans serveur PHP. Nous n'aurons donc pas besoin de prévoir de mise à jour tant que nous ne faisons pas de changements importants.
+
+## URL du site
+Pas encore définies.
+
+## Serveur 
+Pas encore définies.
--- data.php
+++ data.php
@@ -1,21 +1,38 @@
 <?php 
 /*
-ici se trouve la liste des contenus du webdoc
+ici se trouve les contenus du webdoc VIEW305
 */
 
 
-$chap_01["grande_img"]="";
-$chap_01["titre"]="L'Atelier / TRANS305 s'installe au coeur du chantier";
-$chap_01["video_intro"]["youtube"]="iwcRbM6FfN8"; // // video youtube - code de la vidéo
+/* MENU */
+$menu[1]="1.montage";
+$menu[2]="2.usage";
+$menu[3]="3.démontage";
 
+/* CHAPITRE 1 ---------------------------------------------- */
+
+// grande image
+$chap_01["grande_img"]="img/home_view305_img_01.jpg";
+
+// texte
+$chap_01["titre"]="L'Atelier / TRANS305 s'installe au coeur du chantier";
+
+// video
+$chap_01["video"]["youtube"]="iwcRbM6FfN8"; // // video youtube - code de la vidéo
+
+// liste des boutons
 $chap_01[vignette][1]["titre"]="Livreur de prédalle";
 $chap_01[vignette][1]["sous-titre"]="La sécurité, t'as compris ?";
-$chap_01[vignette][1]["slider"]="chap01/img_slider_predalle/";
 $chap_01[vignette][1]["couleur"]="";
+$chap_01[vignette][1]["slider"]="chap01/img_slider_predalle/";
 
 
-$chap_02["video_intro"]["titre"]="Le montage est terminé. L'atelier accueil habitants, étudiants et artistes";
-$chap_02["video_intro"]["youtube"]="eF5r25aqZTw";
+
+/* CHAPITRE 2 ---------------------------------------------- */
+
+$chap_02["grande_img"]="img/chap1/bg_chap1_1.jpg";
+$chap_02["titre"]="Le montage est terminé. L'atelier accueil habitants, étudiants et artistes";
+$chap_02["video"]["youtube"]="eF5r25aqZTw";
 
 
 $chap_02[vignette][1]["titre"]="Effet cocktail party";
@@ -24,8 +41,7 @@ $chap_02[vignette][1]["youtube"]="";
 $chap_02[vignette][1]["couleur"]="";
 
 
-
-
+/* CHAPITRE 3 ---------------------------------------------- */
 $chap_03["video_intro"]="F2HXAoVd-XA";
 
 
