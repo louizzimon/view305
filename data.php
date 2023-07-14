cb797cec0d911070514e54407f74ce53774ff9f3
idfabrik <idfabrik@gmail.com>
2023-07-14 12:30:37 +0200

    Create data.php

---
+++ data.php
@@ -0,0 +1,26 @@
+<?php 
+/*
+ici se trouve la liste des contenus du webdoc
+*/
+
+
+$chap_01["grande_img"]="";
+$chap_01["titre"]="L'Atelier / TRANS305 s'installe au coeur du chantier";
+$chap_01["video_intro"]["youtube"]="iwcRbM6FfN8"; // // video youtube - code de la vidéo
+
+$chap_01[vignette][1]["titre"]="Livreur de prédalle";
+$chap_01[vignette][1]["video"]="";
+
+$chap_01[vignette][1]["titre"]="Livreur de prédalle";
+$chap_01[vignette][1]["sous-titre"]="La sécurité, t'as compris ?";
+$chap_01[vignette][1]["slider"]="chap01/img_slider_predalle/";
+
+
+
+$chap_02["video_intro"]["titre"]="Le montage est terminé. L'atelier accueil habitants, étudiants et artistes";
+$chap_02["video_intro"]["youtube"]="eF5r25aqZTw";
+$chap_03["video_intro"]="F2HXAoVd-XA";
+
+
+
+?>
